ad3fd4b809356ff7bc9928da8833ccce07d1c2a2
61131114002 <43134332+61131114002@users.noreply.github.com>
2019-04-28 10:54:46 +0700

    Ice

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1556423594,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/game-sheet0.png",
+		"images/game-sheet1.png",
+		"images/item-sheet0.png",
+		"images/item-sheet1.png",
+		"images/itemm-sheet0.png",
+		"media/เพลง มาริโอ้.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
